7bf9f98fccf019be5c391fff35ef6d5fa986ae81
Florante Maestrado <florantemaestrado10@gmail.com>
2026-09-11 00:43:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
   Set API_URL to your deployed Google Apps Script Web App URL.
   Do not put a Google service-account key or private secret in this file.
 */
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbxTg_xYKpvMUltYmT_cJ010-79lfZcfBbvVFZnv22_mcOtIvEzi4Jgp0d71Br1Kyb-TXg/exec";
 let session = null;
 let products = [];
 let movements = [];
